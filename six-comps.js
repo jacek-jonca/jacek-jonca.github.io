b91a878531b0dc7d4160ce4e49d2dffe08973f27
Jacek Jonca-Jasinski <jacek@jonca.org>
2019-02-02 14:22:14 -0600

    lottie 6 comps text

---
+++ six-comps.js
@@ -0,0 +1,8 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: 'six-comps.json', // Required
+  renderer: 'svg', // Required
+  loop: false, // Optional
+  autoplay: true, // Optional
+  name: "Hello World", // Name for future reference. Optional.
+})
