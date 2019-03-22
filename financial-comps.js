c21a3814804a712ca360676ca803b002c601533f
Jacek Jonca-Jasinski <jacek@jonca.org>
2019-03-22 17:55:08 -0500

    refresh

--- financial-comps.js
+++
@@ -1,8 +0,0 @@
-var animation = lottie.loadAnimation({
-  container: document.getElementById('lottie'), // Required
-  path: 'financial-comps.json', // Required
-  renderer: 'svg', // Required
-  loop: false, // Optional
-  autoplay: true, // Optional
-  name: "Hello World", // Name for future reference. Optional.
-})
